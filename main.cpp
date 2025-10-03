b65773456ce49b3e74a9094266afd82597ceb0d5
A Student <student@nowhere.edu>
2025-10-03 03:14:47 +0000

    Oct 3, 2025, 3:14 AM

--- main.cpp
+++ main.cpp
@@ -5,16 +5,17 @@ using namespace std;
 int main()
 {
   int teaspoons;
-  cout << "Enter the number of teaspoons: "; //user inputs an integer number of teaspoons
+  cout << "Enter the number of teaspoons: "; // user inputs an integer number of
+                                             // teaspoons
   cin >> teaspoons;
 
-  int tablespoons = teaspoons / 3; //do conversions
+  int tablespoons = teaspoons / 3; // do conversions
   teaspoons = teaspoons % 3;
   int cups = tablespoons / 16;
   tablespoons = tablespoons % 16;
   int quarts = cups / 4;
   cups = cups % 4;
 
-
-  cout << quarts << " quart(s)" << cups << " cup(s)" << tablespoons << " tablespoons(s)" ;
+  cout << quarts << " quart(s)" << cups << " cup(s)" << tablespoons
+       << " tablespoons(s)";
 }
